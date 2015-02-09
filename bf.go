8df7c14cc60fb8ec7361546491ccbd2217604970
phreaker2600 <swwiley@gmail.com>
2015-02-09 01:21:21 +0000

    wat

--- bf.go
+++ bf.go
@@ -32,6 +32,16 @@ func (vm *bfm) create(ssize int, tsize int, program string) {
 	vm.cpu.pc = 0
 	vm.cpu.halted = false
 }
+
+func (vm *bfm) skip() {
+	for vm.program[vm.cpu.pc] != ']' {
+		vm.cpu.pc += 1
+		if vm.program[vm.cpu.pc] == '[' {
+			vm.skip()
+		}
+	}
+	vm.cpu.lc -= 1
+}
 func (vm *bfm) ex(nm rune) {
 	switch nm {
 	case '+':
@@ -40,20 +50,35 @@ func (vm *bfm) ex(nm rune) {
 		vm.tape[vm.cpu.dc] -= 1
 	case '.':
 		fmt.Print(int(vm.tape[vm.cpu.dc]))
-	default:
-		fmt.Println("invalid instrucion" + string(nm))
+	case '<':
+		vm.cpu.dc -= 1
+	case '>':
+		vm.cpu.dc += 1
+	case '[':
+		if vm.tape[vm.cpu.dc] == 0 {
+			vm.skip()
+		}
+		vm.cpu.lc += 1
+		vm.stack[vm.cpu.lc] = vm.cpu.pc
+	case ']':
+		vm.cpu.pc = vm.stack[vm.cpu.lc]
+		vm.cpu.lc -= 1
+
 	}
 	vm.cpu.pc += 1
-
 }
-func (vm *bfm) step() {
-	vm.ex(rune(vm.program[vm.cpu.pc]))
+func (vm *bfm) step() bool {
+	vm.cpu.halted = (vm.cpu.pc >= len(vm.program))
+	if !vm.cpu.halted {
+		vm.ex(rune(vm.program[vm.cpu.pc])) //TODO:UTF8
+	}
+	return vm.cpu.halted
 }
 func main() {
 	vm := new(bfm)
-	vm.create(1000, 1000, "+.")
-	vm.step()
-	vm.step()
+	vm.create(1000, 1000, "+++++.[-].")
+	for !vm.step() {
+	}
 	vm.cpu.status()
 
 }
