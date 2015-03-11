a8df8ec7b80ccdc4bffa120f882264d095822141
phreaker2600 <swwiley@gmail.com>
2015-03-11 15:12:23 +0000

    mnp builds

--- mnp.go
+++ mnp.go
@@ -5,88 +5,81 @@ import "fmt"
 //import "io/ioutil"
 //import "strconv"
 
-type ophandle (vm *task) func()
-
 type alu struct {
 	pc     int
 	lc     int
-	ac	int
+	ac     int
 	halted bool
-	opmap	[]ophandle
+	opmap  []ophandle
 }
 
 func (cpu alu) status() {
-	fmt.Printf("%d\t  %d\t  %d\n", cpu.dc, cpu.pc, cpu.lc)
+	//fmt.Printf("%d\t  %d\t  %d\n", cpu.dc, cpu.pc, cpu.lc)
 }
 
 type task struct {
-	cpu     alu
-	callstack   []int
-	acumstack	[]int
-	ldata []int
-	program string
+	cpu       alu
+	callstack []int
+	acumstack []int
+	ldata     []int
+	program   string
 }
+type ophandle func(*task)
+
+func nop(vm *task) {
+	vm.cpu.pc += 1
+}
+func (cpu *alu) initopmap() {
+	cpu.opmap = make([]ophandle, int('~'))
+	var i = 0
+	for i = 0; i < len(cpu.opmap); i++ {
+		cpu.opmap[i] = nop
+	}
+}
+
 type otask struct {
-	id int
-	name string
+	id    int
+	name  string
 	tasks []int //list of task ids sorted by priority
 
 }
 type system struct {
 	ostate otask
 	states []otask
-
 }
+
 func (vm *task) create(ssize int, tsize int, program string) {
 	vm.callstack = make([]int, ssize)
 	vm.acumstack = make([]int, ssize)
 	vm.ldata = make([]int, tsize)
 	vm.program = program
+	vm.cpu.initopmap()
 	vm.cpu.lc = 0
 	vm.cpu.ac = 0
 	vm.cpu.pc = 0
 	vm.cpu.halted = false
 }
-func (vm *bfm) ex(nm rune) {
-	switch nm {
-	case '+':
-		vm.tape[vm.cpu.dc] += 1
-	case '-':
-		vm.tape[vm.cpu.dc] -= 1
-	case '.':
-		//fmt.Print(int(vm.tape[vm.cpu.dc]))
-	case '<':
-		vm.cpu.dc -= 1
-	case '>':
-		vm.cpu.dc += 1
-	case '[':
-		vm.cpu.lc += 1
-		vm.stack[vm.cpu.lc] = vm.cpu.pc - 1
-		if vm.tape[vm.cpu.dc] == 0 {
-			vm.skip()
-		}
-	case ']':
-		vm.cpu.pc = vm.stack[vm.cpu.lc]
-		vm.cpu.lc -= 1
-
+func (vm *task) ex(nm rune) {
+	var op = int(nm)
+	if op < len(vm.cpu.opmap) {
+		vm.cpu.opmap[op](vm)
 	}
-	vm.cpu.pc += 1
 }
-func (vm *bfm) step() bool {
+func (vm *task) step() bool {
 	vm.cpu.halted = (vm.cpu.pc >= len(vm.program)-1)
 	if !vm.cpu.halted {
 		vm.ex(rune(vm.program[vm.cpu.pc])) //TODO:UTF8
 	}
 	return vm.cpu.halted
 }
-func (vm bfm) status() {
+func (vm task) status() {
 	if !vm.cpu.halted {
-		fmt.Printf("%d\t %d\t %c\t ", vm.stack[vm.cpu.lc], vm.tape[vm.cpu.dc], vm.program[vm.cpu.pc])
+		fmt.Printf("%d\t %d\t %c\t ", vm.callstack[vm.cpu.lc], vm.ldata[vm.cpu.pc], vm.program[vm.cpu.pc])
 		vm.cpu.status()
 	}
 }
 func main() {
-	vm := new(bfm)
+	vm := new(task)
 	vm.create(1000, 1000, "+++++.-.++++++.[-.]+.")
 	for !vm.step() {
 		vm.status()
